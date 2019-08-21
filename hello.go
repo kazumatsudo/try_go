64a49ef94c2f4a2f38fa04cc71ec46ad3bf7089e
kazumatsudo <kazuma.matsudo@icloud.com>
2019-08-21 20:50:52 +0900

    レシーバ

--- hello.go
+++ hello.go
@@ -7,10 +7,23 @@ type user struct {
 	age  int
 }
 
+func (u user) show() {
+	fmt.Printf("name: %s, age: %d\n", u.name, u.age)
+}
+
+func (u *user) incrementAge() {
+	u.age++
+}
+
 func main() {
 	u1 := new(user)
 	u1.age = 10
 	u1.name = "taro"
 
-	fmt.Println(u1)
+	// name: taro, age: 10
+	u1.show()
+
+	u1.incrementAge()
+	// name: taro, age: 11
+	u1.show()
 }
